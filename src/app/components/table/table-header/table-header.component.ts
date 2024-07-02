ba13fc23683ed251803ae7e2c5f437aa9d8d3c17
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2024-07-02 14:49:48 +0300

    added table col sort buttons

--- src/app/components/table/table-header/table-header.component.ts
+++ src/app/components/table/table-header/table-header.component.ts
@@ -15,11 +15,18 @@ import { MatButtonModule } from '@angular/material/button';
 export class TableHeaderComponent {
   headerText = input<string>();
   addFilter = input<boolean>();
+  addSort = input<boolean>();
   filterClicked = output();
   isOpen = false;
+  isSorted = false;
+  sortUp = false;
 
   onFilterClick() {
     this.isOpen = !this.isOpen;
     this.filterClicked.emit();
+  }
+
+  onSortClick() {
+    this.sortUp = !this.sortUp;
   }
 }
