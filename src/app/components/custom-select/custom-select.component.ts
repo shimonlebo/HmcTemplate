91d9ea317079f94317b2fb7538eee2a1b033ecb9
shimonlebo <58909312+shimonlebo@users.noreply.github.com>
2024-07-28 18:05:15 +0300

    add table tags

--- src/app/components/custom-select/custom-select.component.ts
+++ src/app/components/custom-select/custom-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { OverlayModule } from '@angular/cdk/overlay'; 
 import { MatIconModule } from '@angular/material/icon';
 
@@ -10,36 +10,9 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './custom-select.component.scss'
 })
 export class CustomSelectComponent {
-  @Input() options: { value: any; label: string }[] = [];
-  @Input() placeholder: string = 'Select an option';
-  @Output() selectionChange = new EventEmitter<any>();
-
   isOpen = false;
-  selectedOption: { value: any; label: string } | null = null;
 
   toggleOpen(): void {
     this.isOpen = !this.isOpen;
   }
-
-  onOptionSelected(option: { value: any; label: string }): void {
-    this.selectedOption = option;
-    this.isOpen = false;
-    this.selectionChange.emit(option.value);
-  }
-
-  ngOnInit() {
-    document.addEventListener('click', this.onDocumentClick.bind(this));
-  }
-
-  ngOnDestroy() {
-    document.removeEventListener('click', this.onDocumentClick.bind(this));
-  }
-
-  private onDocumentClick(event: MouseEvent): void {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
-      this.isOpen = false;
-    }
-  }
-
-  constructor(private elementRef: ElementRef) {}
 }
